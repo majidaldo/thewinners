29cf861053380c232981dd0340e05fe6e0c969c7
majidaldo <majidaldosar-github@yahoo.com>
2013-10-16 00:10:47 -0400

    corr

---
+++ corrs.py
@@ -0,0 +1,5 @@
+import h5py
+from data import fns
+
+h5f=h5py.File(fns['data'],'r')
+
--- data.py
+++ data.py
@@ -1,2 +1,14 @@
-inputdb='data.hdf5'
-results={'majid':'msdresults.hdf5'}
+import os
+
+datadir='data'
+
+#files in datadir
+fns=\
+{'data':'data.hdf5'
+,'majid':'msdresults.hdf5'
+#,'seqids':'seqids.txt'
+#,'devids':'devids.txt'
+}
+
+for an,af in fns.iteritems(): fns[an]=os.path.join(datadir,af)
+
